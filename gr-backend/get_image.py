e293f5716912efe2abe03d99ea4e4a2f385641bc
Dilan Fernando <mdilanfdo1995@gmail.com>
2024-03-24 01:07:43 +0200

    return list for get image

--- gr-backend/get_image.py
+++ gr-backend/get_image.py
@@ -1,25 +1,23 @@
 import json
 
-def get_image_from_userId(user_id, image_id):
+def get_image_from_userId(user_id):
     with open("output.json", "r") as file:
         output_data = json.load(file)
 
-    image_url = find_image_url_by_id(output_data, user_id, image_id)
+    image_url = find_image_url_by_id(output_data, user_id)
     if image_url is None:
         return {"status": "FAIL"}
     else:
         return {"status": "OK", "image_url": image_url}
 
 
-def find_image_url_by_id(data, target_id, image_id):
+def find_image_url_by_id(data, target_id):
+    lst = []
     for item in data:
         if item['id'] == target_id:
-            if image_id == 1:
-                return item['image_url_1']
-            elif image_id == 2:
-                return item['image_url_2']
-            else:
-                return None
+            lst.append(item['image_url_1'])
+            lst.append(item['image_url_2'])
+            return lst
     return None
 
 
