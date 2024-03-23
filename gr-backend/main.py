8d4bc0eb5f049fad43bc4fa8805ee4ce5823d4af
Dilafdo <mdilanfdo1995@gmail.com>
2024-03-23 14:56:41 +0200

    create prompt.py

--- gr-backend/main.py
+++ gr-backend/main.py
@@ -3,33 +3,33 @@ from typing import Optional
 
 app = FastAPI()
 
-students = {
-    1: {
-        "name": "John",
-        "age": 14,
-        "class": "year 12"
-    }
-}
+# students = {
+#     1: {
+#         "name": "John",
+#         "age": 14,
+#         "class": "year 12"
+#     }
+# }
 
 @app.get("/")
 def index():
-    return {"name": "First name"}
+    return {"App is working!!"}
 
-@app.get("/students/{student_id}")
-def get_student(student_id: int = Path(description="Id of the student", gt=0, lt=3)):
+@app.post("post-prompt")
+def post_prompt(user_id: int, prompt: str, gendre: str):
     return students[student_id]
 
-@app.get("/students-by-name")
-def get_student_by_name(name: Optional[str] =  None):
-    for student_id in students:
-        if students[student_id]["name"] == name:
-            return students[student_id]
-    return {"data": "not found"}
-
-## create student endpoint
-@app.post("/create-student/{student_id}")
-def create_student(student_id: int, student: dict):
-    if student_id in students:
-        return {"Error": "Student exists"}
-    students[student_id] = student
-    return students[student_id]
+# @app.get("/students-by-name")
+# def get_student_by_name(name: Optional[str] =  None):
+#     for student_id in students:
+#         if students[student_id]["name"] == name:
+#             return students[student_id]
+#     return {"data": "not found"}
+#
+# ## create student endpoint
+# @app.post("/create-student/{student_id}")
+# def create_student(student_id: int, student: dict):
+#     if student_id in students:
+#         return {"Error": "Student exists"}
+#     students[student_id] = student
+#     return students[student_id]
