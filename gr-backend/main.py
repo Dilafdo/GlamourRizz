0a482a883e25a897de9eb175be7bb372a424ff89
Dilan Fernando <mdilanfdo1995@gmail.com>
2024-03-23 18:48:27 +0200

    added post request

--- gr-backend/main.py
+++ gr-backend/main.py
@@ -1,18 +1,23 @@
 from fastapi import FastAPI, Path
 from typing import Optional
 from image_generation import generate_image
+from prompt import prompt
 
 
 app = FastAPI()
 
-@app.get("/generate-image")
-def index():
-    image_path = generate_image("dalle/data/input/sydney.png", prompt = "A girl img wearing a blue dress, CGI, realistic, fhalfull-body, screenshot from party")
-    return {"name": image_path}
+# @app.get("/generate-image")
+# def index():
+#     image_path = generate_image("https://glamourizz.s3.eu-north-1.amazonaws.com/trouser/denim-men-llight.webp", prompt = "A girl img wearing a blue dress, CGI, realistic, fhalfull-body, screenshot from party")
+#     return {"name": image_path}
 
-@app.post("post-prompt")
-def post_prompt(user_id: int, prompt: str, gendre: str):
-    return {"success": "Ok"}
+# @app.get("/hello")
+# def hello():
+#     return {"name": "hello world"}
+
+@app.post("/post-prompt")
+def post_prompt(data: dict):
+    return prompt(data["user_id"], data["prompt"], data["gender"])
 
 def main():
     images = generate_image("dalle/data/input/sydney.png", prompt = "A girl img wearing a blue dress, CGI, realistic, fhalfull-body, screenshot from party")
--- gr-backend/prompt.py
+++ gr-backend/prompt.py
@@ -1,2 +1,29 @@
+from openapi import generate_ids
+from image_generation import generate_image
+import json
 def prompt(user_id, prompt, gender):
-    print("Prompting user")
+    with open("bottom.json", "r") as file:
+        bottom_data = json.load(file)
+
+    with open("top.json", "r") as file:
+        top_data = json.load(file)
+
+    with open("jewellery.json", "r") as file:
+        jewellery_data = json.load(file)
+
+    data_obj = {
+        "bottom": bottom_data,
+        "top": top_data,
+        "jewellery": jewellery_data
+    }
+
+    # Find the matching ids
+    result = generate_ids(data_obj, prompt, gender)
+    while result["status"] == "FAIL":
+        result = generate_ids(data_obj, prompt, gender)
+    # print(user_id)
+    # print(result)
+    generate_image(image_path, result["new_prompt"])
+
+    return result
+
