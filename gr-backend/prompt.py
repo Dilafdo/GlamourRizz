c847906266c444b92d588b11e0be2c97a596ca3d
Dilan Fernando <mdilanfdo1995@gmail.com>
2024-03-23 21:19:10 +0200

    update 2

--- gr-backend/prompt.py
+++ gr-backend/prompt.py
@@ -12,6 +12,9 @@ def prompt(user_id, prompt, gender, image_url):
     with open("jewellery.json", "r") as file:
         jewellery_data = json.load(file)
 
+    with open("output.json", "r") as file:
+        output_data = json.load(file)
+
     data_obj = {
         "bottom": bottom_data,
         "top": top_data,
@@ -31,9 +34,24 @@ def prompt(user_id, prompt, gender, image_url):
     # Remove the file extension
     name = os.path.splitext(filename)[0]
 
-    prompt_str = f"This is , {name}. {result['new_prompt']}"
+    prompt_str = f"This is {name}. {result['new_prompt']}"
+    print(prompt_str)
     image_path = generate_image(image_url, prompt_str)
-    return {"name": image_path}
+
+    # Create JSON object
+    json_object = {
+        "id": user_id,
+        "image_url_1": image_path[0],
+        "image_url_2": image_path[1]
+    }
+
+    print("JSON Object: ", json_object)
+
+    output_data.append(json_object)
+    with open("output.json", "w") as file:
+        json.dump(output_data, file, indent=4)
+
+    # return {"name": image_path}
 
     # return result
 
