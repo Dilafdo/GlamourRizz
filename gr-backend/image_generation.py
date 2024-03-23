b425312b8627605ccb59a90b0f6d6f88a71df6c4
Rukshan Perera <2173530+KRVPerera@users.noreply.github.com>
2024-03-23 23:37:56 +0200

    fix: generate image path

--- gr-backend/image_generation.py
+++ gr-backend/image_generation.py
@@ -30,3 +30,29 @@ def generate_image(image_link, prompt):
     )
 
     return output
+
+
+def generate_image_path(image_path, prompt):
+    # Load image
+    # Download image
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    # Generate image
+    resize1 = cv2.resize(image, (0, 0), fx = 0.1, fy = 0.1)
+    PREPRCOESSED_IMAGE_PATH = "data/out/temp.png"
+    cv2.imwrite(PREPRCOESSED_IMAGE_PATH, resize1)
+    gray = open(PREPRCOESSED_IMAGE_PATH, "rb")
+    output = api.run(
+        "tencentarc/photomaker-style:467d062309da518648ba89d226490e02b8ed09b5abc15026e54e31c5a8cd0769",
+        input={
+            "prompt": prompt + "CGI, realistic, half-body",
+            "num_steps": 25,
+            "style_name": "Photographic (Default)",
+            "input_image": gray,
+            "num_outputs": 2,
+            "guidance_scale": 3,
+            "negative_prompt": "nsfw, worst quality, greyscale, bad anatomy, bad hands, error, text",
+            "style_strength_ratio": 20
+        }
+    )
+
+    return output
